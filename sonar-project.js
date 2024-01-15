ceade2e3c679ca0518fa638e0cfa6a6d63ae3a44
ARUNBA13U <133482396+ARUNBA13U@users.noreply.github.com>
2024-01-15 08:20:09 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.110.101.192:9000/',
+  serverUrl: 'http://13.201.4.107:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
